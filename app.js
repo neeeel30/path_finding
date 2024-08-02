36afe07f065d3e2c6e7d7a9e23d434350b2aaa38
Neel Patel <115921003+neeeel30@users.noreply.github.com>
2024-08-02 17:03:12 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const locationRoutes = require('./routes/locationRoutes');
+const roadRoutes = require('./routes/roadRoutes');
+const trafficUpdateRoutes = require('./routes/trafficUpdateRoutes');
+const pathfindingRoutes = require('./routes/pathfindingRoutes');
+const reportRoutes = require('./routes/reportRoutes');
+
+const connectDB = require('./utils/db');
+
+const app = express();
+app.use(express.json());
+
+connectDB();
+
+app.use('/locations', locationRoutes);
+app.use('/roads', roadRoutes);
+app.use('/traffic-updates', trafficUpdateRoutes);
+app.use('/shortest-path', pathfindingRoutes);
+app.use('/report', reportRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
